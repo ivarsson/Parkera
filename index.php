8bce678b1d902bd19092598dfd3a718a2b97605d
Johan Ivarsson <johan.ivarsson88@gmail.com>
2011-12-09 11:44:38 +0100

    js

--- index.php
+++ index.php
@@ -13,30 +13,6 @@
 	<script type="text/javascript" src="maps.js"></script>
 	<script type="text/javascript" charset="utf-8">
 	
-	(function() {
-	        var fixgeometry = function() {
-	          /* Some orientation changes leave the scroll position at something
-	           * that isn't 0,0. This is annoying for user experience. */
-	          scroll(0, 0);
-
-	          /* Calculate the geometry that our content area should take */
-	          var header = $(".header:visible");
-	          var footer = $(".footer:visible");
-	          var content = $(".content:visible");
-	          var viewport_height = $(window).height();
-
-	          var content_height = viewport_height - header.outerHeight() - footer.outerHeight();
-
-	          /* Trim margin/border/padding height */
-	          content_height -= (content.outerHeight() - content.height());
-	          content.height(content_height);
-	        }; /* fixgeometry */
-
-	        $(document).ready(function() {
-	          $(window).bind("orientationchange resize pageshow", fixgeometry);
-	        });
-	      })();
-	
 	var timeStore = {
         h:     undefined,
         m:     undefined,
