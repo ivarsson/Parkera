32567c720a8478591aa130f8c8d2ace40eb245fa
Johan Ivarsson <johan.ivarsson88@gmail.com>
2011-12-09 12:54:08 +0100

    trans height

--- publicTrans.php
+++ publicTrans.php
@@ -2,7 +2,7 @@
 	<h1>Närmaste hållplatser</h1>
 </div>
 
-<iframe src="http://lindholmen.heroku.com/" width="100%" height="100%"></iframe>
+<iframe src="http://lindholmen.heroku.com/" width="100%" height="686px"></iframe>
 
 <div data-role="footer" data-theme="a" data-position="fixed">
 	<p>
